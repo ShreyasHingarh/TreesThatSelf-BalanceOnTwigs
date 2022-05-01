b8bccf8aa2009c9519a0c1d4d662c14e9f2fcc72
GMR Student <gmrstudent@outlook.com>
2022-05-01 15:59:04 -0700

    yes

---
+++ Self-BalancingTrees/Self-BalancingTrees/BinaryHeapTree.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Self_BalancingTrees
+{
+    class BinaryHeapTree<T> where T:IComparable<T>
+    {
+        List<T> values = new List<T>();
+        public void Insert(T value)
+        {
+            values.Add(value);
+            HeapifyUp(values.Count - 1);
+        }
+
+        public int GetRightChild(int index)
+        {
+            return index * 2 + 2;
+        }
+        public int GetLeftChild(int index)
+        {
+            return index * 2 + 1;
+        }
+        public int GetParent(int index)
+        {
+            return (index - 1) / 2;
+        }
+        public void HeapifyUp(int index)
+        {
+            while(index != 0)
+            {
+                if (values[index].CompareTo(values[GetParent(index)]) <= 0)
+                {
+                    T temp = values[index];
+                    values[index] = values[GetParent(index)];
+                    values[GetParent(index)] = temp;
+
+                    index = GetParent(index);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+        }
+        
+        public void Print()
+        {
+            for(int i = 0;i < values.Count;i++)
+            {
+                Console.WriteLine(values[i]);
+            }
+        }
+    }
+}
--- Self-BalancingTrees/Self-BalancingTrees/Program.cs
+++ Self-BalancingTrees/Self-BalancingTrees/Program.cs
@@ -6,7 +6,13 @@ namespace Self_BalancingTrees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryHeapTree<int> tree = new BinaryHeapTree<int>();
+
+            for(int i = 0;i < 10;i++)
+            {
+                tree.Insert(i);
+            }
+            tree.Print();
         }
     }
 }
