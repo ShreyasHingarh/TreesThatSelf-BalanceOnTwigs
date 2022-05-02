4b349d467ed3ab068dbdf57bb2aa2eef078f5aae
GMR Student <gmrstudent@outlook.com>
2022-05-01 17:01:28 -0700

    made pop

--- Self-BalancingTrees/Self-BalancingTrees/BinaryHeapTree.cs
+++ Self-BalancingTrees/Self-BalancingTrees/BinaryHeapTree.cs
@@ -6,7 +6,7 @@ namespace Self_BalancingTrees
 {
     class BinaryHeapTree<T> where T:IComparable<T>
     {
-        List<T> values = new List<T>();
+        public List<T> values { get; } = new List<T>();
         public void Insert(T value)
         {
             values.Add(value);
@@ -45,6 +45,51 @@ namespace Self_BalancingTrees
             
         }
         
+        public T Pop()
+        {
+            T temp = values[0];
+            values[0] = values[values.Count - 1];
+            values.Remove(values[values.Count - 1]);
+            HeapifyDown(0);
+            return temp;
+        }
+        public void HeapifyDown(int index)
+        {
+            while(true)
+            {
+                int rIndex = GetRightChild(index);
+                int lIndex = GetLeftChild(index);
+                if (lIndex >= values.Count && rIndex >= values.Count)
+                {
+                    break;
+                }
+
+                //Figure out which child is smaller
+                //Check if the smaller child is smaller than the parent
+                int swapIndex = rIndex;
+                if(rIndex >= values.Count)
+                {
+                    swapIndex = lIndex;
+                }
+                else if(values[lIndex].CompareTo(values[rIndex]) < 0)//Both indexes must be in the array
+                {
+                    swapIndex = lIndex;
+                }
+                
+
+                if(values[swapIndex].CompareTo(values[index]) < 0)
+                {
+                    T temp = values[index];
+                    values[index] = values[swapIndex];
+                    values[swapIndex] = temp;
+                    index = swapIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
         public void Print()
         {
             for(int i = 0;i < values.Count;i++)
--- Self-BalancingTrees/Self-BalancingTrees/Program.cs
+++ Self-BalancingTrees/Self-BalancingTrees/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Self_BalancingTrees
 {
@@ -8,11 +9,48 @@ namespace Self_BalancingTrees
         {
             BinaryHeapTree<int> tree = new BinaryHeapTree<int>();
 
-            for(int i = 0;i < 10;i++)
+            Random rand = new Random();
+
+            int[] nums = new int[20];// { 4,5,1,3,2};
+            for (int i = 0; i < nums.Length; i++)
             {
-                tree.Insert(i);
+                nums[i] = rand.Next(1, 100);
             }
-            tree.Print();
+
+            for (int i = 0;i < nums.Length;i++)
+            {
+                tree.Insert(nums[i]);
+            }
+            Console.WriteLine(IsValid(tree));
+            List<int> s = new List<int>();
+            while(tree.values.Count > 0)
+            {
+                s.Add(tree.Pop());
+            }
         }
+
+        static bool IsValid(BinaryHeapTree<int> heap)
+        {
+            return IsValid(heap, 0); 
+        }
+            
+        static bool IsValid(BinaryHeapTree<int> heap, int index)
+        {
+            if(index >= heap.values.Count)
+            {
+                return true;
+            }
+
+            //Store curvalue, leftvalue,rightvalue
+            int cur = heap.values[index];
+            int lIndex = heap.GetLeftChild(index);
+            int rIndex = heap.GetRightChild(index);
+            if (lIndex < heap.values.Count && cur > heap.values[lIndex]) return false;
+            if (rIndex < heap.values.Count && cur > heap.values[rIndex]) return false; 
+            
+            return IsValid(heap,index+1);
+        }
+
+
     }
 }
